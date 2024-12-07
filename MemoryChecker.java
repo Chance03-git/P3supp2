55ed9cb5fa7f6765b65789abdf77cf9db7b5391b
Chance03-git <Chancefoster05@gmail.com>
2024-12-06 23:25:50 -0500

    Function defined for hasEnoughMemory

--- MemoryChecker.java
+++ MemoryChecker.java
@@ -1,4 +1,9 @@
 public class MemoryChecker {
+    public static boolean hasEnoughMemory(int requiredMB) {
+        long maxMemoryBytes = Runtime.getRuntime().maxMemory();
+        long requiredBytes = (long) requiredMB * 1024 * 1024; // Convert MB to bytes
+        return requiredBytes <= maxMemoryBytes;
+    }
     public static void main(String[] args) {
         // Test cases for the hasEnoughMemory() function
 
